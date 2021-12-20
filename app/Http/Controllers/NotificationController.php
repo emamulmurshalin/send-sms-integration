3b92427697d876e5dcfce11cae872d0f312f4cb4
“Emamul” <emamul727@gmail.com>
2021-12-21 00:55:06 +0600

    sms send

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -18,7 +18,7 @@ class NotificationController extends Controller
         $client = new \Vonage\Client($basic);
 
         $response = $client->sms()->send(
-            new \Vonage\SMS\Message\SMS("8801777822162", BRAND_NAME, 'A text message sent using the Nexmo SMS API')
+            new \Vonage\SMS\Message\SMS("8801721253636", BRAND_NAME, 'A text message sent using the Nexmo SMS API')
         );
 
         $message = $response->current();
